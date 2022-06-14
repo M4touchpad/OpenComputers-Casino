a7a519f3c0e856ab78c81f53cb3f2c259db0d0ea
M4_touchpad <wenom232@gmail.com>
2022-06-14 16:39:27 +0700

    изменил на свой гитхаб

--- install.lua
+++ install.lua
@@ -1,4 +1,4 @@
-local REPOSITOTY = "https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino/master"
+local REPOSITOTY = "https://raw.githubusercontent.com/M4_touchpad/OpenComputers-Casino/main"
 
 local shell = require("shell")
 shell.execute("wget -fq " .. REPOSITOTY .. "/launcher.lua /home/1.lua")
