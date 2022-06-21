9bc4698231641a953219bd11b257c7f3a5aa2807
M4touchpad <103248078+M4touchpad@users.noreply.github.com>
2022-06-21 18:17:13 +0700

    Оставил только основную валюту

--- config/currencies.lua
+++ config/currencies.lua
@@ -14,9 +14,9 @@ addCurrency("Деньги",           "customnpcs:npcMoney",      0,   'INGOT', 
 --addCurrency("Светопыль",        "minecraft:glowstone_dust", 0,   'DUST',  0xD0D000, 5)
 --addCurrency("Железный слиток",  "minecraft:iron_ingot",     0,   'INGOT', 0xAAAAAA, nil)
 addCurrency("Железный блок",    "minecraft:iron_block",     0,   'BLOCK', 0xAAAAAA, 6)
-addCurrency("Медный слиток",    "IC2:itemIngot",            0,   'INGOT', 0xA5642F, nil)
+--addCurrency("Медный слиток",    "IC2:itemIngot",            0,   'INGOT', 0xA5642F, nil)
 --addCurrency("Медный блок",      "Forestry:resourceStorage", 1,   'BLOCK', 0xA5642F, 6)
-addCurrency("Оловянный слиток", "IC2:itemIngot",            1,   'INGOT', 0xCCCCCC, nil)
+--addCurrency("Оловянный слиток", "IC2:itemIngot",            1,   'INGOT', 0xCCCCCC, nil)
 --addCurrency("Оловянный блок",   "Forestry:resourceStorage", 2,   'BLOCK', 0xCCCCCC, 6)
 --addCurrency("Бесплатно",        nil,                        nil, nil,     0xE5E5E5, nil)
 
